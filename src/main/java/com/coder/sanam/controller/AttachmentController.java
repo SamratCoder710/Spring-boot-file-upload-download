149f76e7de61e1d6c09f63d05f67b89421902945
SamratCoder710 <shubhamksrathod26@gmail.com>
2023-06-26 01:51:40 +0530

    Adder fileStorage Option

--- src/main/java/com/coder/sanam/controller/AttachmentController.java
+++ src/main/java/com/coder/sanam/controller/AttachmentController.java
@@ -1,5 +1,13 @@
 package com.coder.sanam.controller;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Path;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
@@ -17,35 +25,98 @@ import com.coder.sanam.ResponseData;
 import com.coder.sanam.entity.Attachment;
 import com.coder.sanam.service.AttachmentService;
 
+import jakarta.servlet.http.HttpServletResponse;
+
 @RestController
 public class AttachmentController {
 
 	private AttachmentService attachmentService;
+
+	@Value("${fileStorage.enabled}")
+	private boolean fileStorageEnabled;
 
 	public AttachmentController(AttachmentService attachmentService) {
 		this.attachmentService = attachmentService;
 	}
 
 	@PostMapping("/uploadFile")
-	public ResponseData uploadFile(@RequestParam("file") MultipartFile file) {
-		Attachment attachment = null;
+	public ResponseData uploadFile(@RequestParam("file") MultipartFile file) throws IllegalStateException, IOException {
 		String downloadURL = "";
-		System.out.println(file.getOriginalFilename());
-		attachment = attachmentService.saveAttachment(file);
-		downloadURL = ServletUriComponentsBuilder.fromCurrentContextPath().path("/download/").path(attachment.getId())
-				.toUriString();
+		long fileSize = 0;
+		String fileType = "";
+		String fileName = "";
+		if (fileStorageEnabled) {
+			try {
+				String dest = "D:\\Shubham\\data\\";
+				String destFilePath = dest + file.getOriginalFilename();
 
-		return new ResponseData(file.getSize(), downloadURL, attachment.getFileName(), file.getContentType());
+				file.transferTo(new File(destFilePath));
+				System.out.println("File Uploaded successfully!!!");
+				downloadURL = ServletUriComponentsBuilder.fromCurrentContextPath().path("/download/")
+						.path(file.getOriginalFilename()).toUriString();
+				fileSize = file.getSize();
+				fileType = file.getContentType();
+				fileName = file.getOriginalFilename();
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+		} else {
+
+			Attachment attachment = null;
+			System.out.println(file.getOriginalFilename());
+			attachment = attachmentService.saveAttachment(file);
+			downloadURL = ServletUriComponentsBuilder.fromCurrentContextPath().path("/download/")
+					.path(attachment.getId()).toUriString();
+			fileSize = file.getSize();
+			fileType = file.getContentType();
+			fileName = attachment.getFileName();
+
+		}
+		return new ResponseData(fileSize, downloadURL, fileName, fileType);
 
 	}
 
 	@GetMapping("/download/{fileId}")
-	public ResponseEntity<Resource> downloadAttachment(@PathVariable String fileId) {
-		Attachment attachment = null;
-		attachment = attachmentService.getAttachmentById(fileId);
-		return ResponseEntity.ok().contentType(MediaType.parseMediaType(attachment.getFileType()))
-				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + attachment.getFileName() + "\"")
-				.body(new ByteArrayResource(attachment.getData()));
+	public void downloadAttachment(@PathVariable String fileId, HttpServletResponse response) throws IOException {
+		if(fileStorageEnabled) {
+			String filepath = "D:\\Shubham\\data\\" + fileId;
+			File file = new File(filepath);
+			FileInputStream fileInputStream = new FileInputStream(new File(filepath));
+			response.setContentType("application/octet-stream");
+			response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment ; filename = \"" + file.getName() + "\"");
+			OutputStream outputStream = response.getOutputStream();
+
+			byte[] buffer = new byte[4096];
+			int bytesRead;
+			while ((bytesRead = fileInputStream.read(buffer)) != -1) {
+				outputStream.write(buffer, 0, bytesRead);
+			}
+			fileInputStream.close();
+			outputStream.close();
+		}else {
+			Attachment attachment = null;
+			attachment = attachmentService.getAttachmentById(fileId);
+			response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment ; filename = \"" + attachment.getFileName()  + "\"");
+			ByteArrayResource byteArrayResource = new ByteArrayResource(attachment.getData());
+			response.setContentType("application/octet-stream");
+
+	        // Set the content length
+	        response.setContentLength((int) byteArrayResource.contentLength());
+
+	        // Set the body content
+	        InputStream inputStream = byteArrayResource.getInputStream();
+	        byte[] buffer = new byte[1024];
+	        int bytesRead;
+	        while ((bytesRead = inputStream.read(buffer)) != -1) {
+	            response.getOutputStream().write(buffer, 0, bytesRead);
+	        }
+	        inputStream.close();
+			
+		}
+		
+
+
+
 	}
 
 }
